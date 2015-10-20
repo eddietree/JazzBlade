dfb1a3afeecb101956acb25efc1633533f4aaee6
Eddie Lee <eddie@funktroniclabs.com>
2015-10-19 23:04:16 -0700

    blade effect setup

---
+++ Unity/Assets/00_Everything/Materials/Shaders/BladeTrail.shader
@@ -0,0 +1,65 @@
+﻿Shader "JazzBlade/BladeTrail"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		//Tags { "RenderType"="Opaque" }
+		Tags { "Queue" = "Transparent" }
+		LOD 100
+		
+
+		Pass
+		{
+			Blend SrcAlpha OneMinusSrcAlpha // Alpha blending
+			Cull  Off
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				float3 normal : TEXCOORD1;
+				
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.normal = v.normal;
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				col.xyz = i.normal;
+				// apply fog
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ Unity/Assets/00_Everything/Scripts/BladeTrail.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BladeTrail : MonoBehaviour 
+{
+    const int TrailNumFrames = 64;
+
+    struct Frame
+    {
+        Vector3 pos0;
+        Vector3 pos1;
+        Quaternion quat;
+    };
+
+    Frame[] frames;
+    int numFrames;
+
+    private Vector3[] newVertices;
+    private Vector2[] newUV;
+    private int[] newTriangles;
+
+	void Start () 
+    {
+        InitFrames();
+	}
+
+    void InitFrames()
+    {
+        frames = new Frame[TrailNumFrames];
+        numFrames = 0;
+        InitMesh();
+    }
+
+    void InitMesh()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        // verts
+        int numVertsPerFrame = 2;
+        int numVertsTotal = numVertsPerFrame * TrailNumFrames;
+
+        // indices
+        int numSegs = TrailNumFrames - 1;
+        int numIndicesPerSeg = 6;
+        int numIndicesTotal = numIndicesPerSeg * numSegs;
+
+        // init arrays
+        newVertices = new Vector3[numVertsTotal];
+        newUV = new Vector2[numVertsTotal];
+        newTriangles = new int[numIndicesTotal];
+        
+        // garbage data
+        newVertices[0] = new Vector3(0.0f, 0.0f, 1.0f);
+        newVertices[1] = new Vector3(1.0f, 0.0f, 0.0f);
+        newVertices[2] = new Vector3(1.0f, 1.0f, 1.0f);
+        newVertices[3] = new Vector3(0.0f, 1.0f, 0.0f);
+
+        // indices
+        for (int i = 0; i < numSegs; ++i )
+        {
+            int indexOffset = i * numIndicesPerSeg;
+            int vertOffset = numVertsPerFrame * i;
+
+            newTriangles[indexOffset + 0] = vertOffset+0;
+            newTriangles[indexOffset + 1] = vertOffset+1;
+            newTriangles[indexOffset + 2] = vertOffset+3;
+
+            newTriangles[indexOffset + 3] = vertOffset + 0;
+            newTriangles[indexOffset + 4] = vertOffset + 2;
+            newTriangles[indexOffset + 5] = vertOffset + 3;
+        }
+
+        mesh.vertices = newVertices;
+        mesh.uv = newUV;
+        mesh.triangles = newTriangles;
+
+        mesh.RecalculateNormals();
+    }
+	
+	void Update () 
+    {
+	
+	}
+}
