4a126023ec8cb6e6a6a6d0c62549f26eea3eec86
Eddie Lee <eddie@funktroniclabs.com>
2015-10-20 23:48:42 -0700

    note playing now

--- Unity/Assets/00_Everything/Scripts/BeatSphere.cs
+++ Unity/Assets/00_Everything/Scripts/BeatSphere.cs
@@ -2,19 +2,37 @@
 using System.Collections;
 using DG.Tweening;
 
-public class BeatSphere : MonoBehaviour {
+public class BeatSphere : MonoBehaviour 
+{
+
+    float PitchShift( int numSemitones )
+    {
+        return Mathf.Pow(1.05946f, numSemitones);
+    }
 
 	void Start () 
     {
         StartCoroutine("HeadTowardsPlayer");
 
+        // scale pulsate
+        float secsPerBeat = GameObject.Find("BPM").GetComponent<BPM>().GetSecsPerBeat();
         transform.DOPunchScale(Vector3.one * 0.1f, secsPerBeat * 0.5f);
+
+        int[] semitones =
+        {
+            0, 4, 7, 11, 12
+        };
+
+        // pitch
+        var audioSrc = GetComponent<AudioSource>();
+        var semitoneIndex = Random.RandomRange(0, semitones.Length);
+        audioSrc.pitch = PitchShift(semitones[semitoneIndex]);
+        audioSrc.Play();
 	}
 	
-	void Update () 
-    {
+	void Update () {
+	
 	}
-
 
     IEnumerator HeadTowardsPlayer()
     {
@@ -27,13 +45,18 @@ public class BeatSphere : MonoBehaviour {
 
         float delayNumBeats = 3.0f;
         
-
         yield return new WaitForSeconds(secsPerBeat * delayNumBeats);
 
         //transform.DOMove(dst, secsPerBeat * (4.0 - delayNumBeats));
         var timeLeft = secsPerBeat * (4-delayNumBeats);
         transform.DOMove(dst, timeLeft).SetEase( Ease.InBack);
 
+        yield return new WaitForSeconds(secsPerBeat);
+
+        var audioSrc = GetComponent<AudioSource>();
+        audioSrc.Play();
+
+        transform.DOPunchScale(Vector3.one * 0.1f, secsPerBeat * 0.5f);
         yield return new WaitForSeconds(secsPerBeat);
         Destroy(gameObject);
     }
